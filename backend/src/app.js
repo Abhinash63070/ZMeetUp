67239aced45d331e223b766de0c9d5f1fba5dea6
Abhinash63070 <152368776+Abhinash63070@users.noreply.github.com>
2025-07-30 12:20:04 +0530

    link added

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,11 @@ const io = connectToSocket(server);
 
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+app.use(cors({
+    origin:"https://zmeetup-frontend-clwm.onrender.com"
+}
+
+));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
